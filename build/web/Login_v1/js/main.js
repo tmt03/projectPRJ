5f41a59afa191ea7c52cfcd020829c27e951a20c
tmt03 <166572154+tmt03@users.noreply.github.com>
2024-05-26 00:50:37 +0700

    Update main.js

--- build/web/Login_v1/js/main.js
+++ build/web/Login_v1/js/main.js
@@ -10,12 +10,13 @@
     $('.validate-form').on('submit',function(){
         var check = true;
 
-for (const item of input) {
-    if (!validate(item)) {
-        showValidate(item);
+for(var i = 0; i < input.length; i++) {
+    if (validate(input[i]) == false) {
+        showValidate(input[i]);
         check = false;
     }
 }
+
 
 
 
