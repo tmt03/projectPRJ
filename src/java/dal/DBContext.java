a98d8b7e5bf787359e6b74a138e214bd1faa93cb
tmt03 <166572154+tmt03@users.noreply.github.com>
2024-05-26 01:47:09 +0700

    Update DBContext.java

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -1,26 +1,28 @@
 package dal;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 public class DBContext {
     protected Connection connection;
-    public DBContext()
-    {
+
+    public DBContext() {
         Properties prop = new Properties();
         try (InputStream input = new FileInputStream("config.properties")) {
             prop.load(input);
             String user = "sa";
             String pass = prop.getProperty("db.password");
             String url = "jdbc:sqlserver://LAPTOP-UACSD3DT:1433;databaseName=QLNS";
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
-        } catch (ClassNotFoundException | SQLException ex) {
+        } catch (IOException | SQLException ex) {
             Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    
-
 }
